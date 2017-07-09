c1b26eb159da46ef563446142d7ac103f0cada1f
romua <romafromukraine@gmail.com>
2017-07-09 18:31:34 +0300

    first commit

---
+++ js/task1.js
@@ -0,0 +1,14 @@
+function task1() {
+	var a = prompt("Enter a:", '');
+	var b = prompt("Enter b:", '');
+	var c = prompt("Enter c:", '');
+
+	var d = b*b-4*a*c;
+
+	var x1 = (d > 0) ? ((-1)*b+Math.sqrt(d))/2*a : "D < 0"; 
+	var x2 = (d > 0) ? ((-1)*b-Math.sqrt(d))/2*a : "D < 0"; 
+	
+	let resultTask1 = `x1 = ${x1} and x2 = ${x2}`;
+
+	return alert(resultTask1);
+}
